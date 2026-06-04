abc192b9c4343ffcf6966b090b7a96db45fcd6c1
prodigy00 <calitoalejandro184@gmail.com>
2026-06-04 19:05:30 -0400

    refactor: remove unused middleware and update PostCSS configuration for Tailwind CSS

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,4 @@
 const config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/features/**/*.{js,ts,jsx,tsx,mdx}", // Asegúrate de agregar las carpetas que uses
-  ],
   plugins: {
     "@tailwindcss/postcss": {},
   },
