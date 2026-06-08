d81735c02fb864f4ebd19e6cd0110745e6bc60a5
prodigy00 <calitoalejandro184@gmail.com>
2026-06-07 21:50:53 -0400

    chore: add barrel exports for consultations and medications features

---
+++ src/features/consultations/index.ts
@@ -0,0 +1,6 @@
+export { ActiveConsultationView } from "./components/ActiveConsultationView";
+export { ClinicalHistoryTimeline } from "./components/ClinicalHistoryTimeline";
+export { ClinicalNotesForm } from "./components/ClinicalNotesForm";
+export { ConsultationTabs } from "./components/ConsultationTabs";
+export { DigitalPrescriptionCard } from "./components/DigitalPrescriptionCard";
+export { PatientContextCard } from "./components/PatientContextCard";
---
+++ src/features/medications/index.ts
@@ -0,0 +1,3 @@
+export { MedicationForm } from "./components/MedicationForm";
+export { MedicationTable } from "./components/MedicationTable";
+export { MedicationsCrudLayout } from "./components/MedicationsCrudLayout";
